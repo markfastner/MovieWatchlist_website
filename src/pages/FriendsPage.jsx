ffce6cdcd4812a7eac60f2dc9947cf6bc35c7b09
Jake Spievak <jakespvk@gmail.com>
2023-02-08 16:53:58 -0800

    wip @lreissmueller

--- src/pages/FriendsPage.jsx
+++ src/pages/FriendsPage.jsx
@@ -1,9 +1,98 @@
-import React from 'react'
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
+import React from 'react';
 
-export default function Friends() {
-    return(
-        <div>
-            <h1>friends page...</h1>
-        </div>
-    )
+class Friends extends React.Component {
+  state = {
+    friends: [],
+    user: null
+  };
+
+  componentDidMount() {
+    // Initialize Firebase
+    firebase.initializeApp({
+      apiKey: 'YOUR_API_KEY',
+      authDomain: 'YOUR_AUTH_DOMAIN',
+      databaseURL: 'YOUR_DATABASE_URL',
+    });
+
+    // Listen for changes to the user's authentication state
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user });
+      } else {
+        this.setState({ user: null });
+      }
+    });
+
+    // Listen for changes to the friends list
+    firebase
+      .database()
+      .ref('friends')
+      .on('value', (snapshot) => {
+        this.setState({ friends: snapshot.val() });
+      });
+  }
+
+  handleAddFriend = (friend) => {
+    // Check if the user is authenticated
+    if (this.state.user) {
+      // Add a new friend to the Firebase database
+      firebase
+        .database()
+        .ref('friends')
+        .push(friend);
+    } else {
+      alert('Please sign in to add a friend.');
+    }
+  };
+
+  handleDeleteFriend = (id) => {
+    // Check if the user is authenticated
+    if (this.state.user) {
+      // Remove a friend from the Firebase database
+      firebase
+        .database()
+        .ref(`friends/${id}`)
+        .remove();
+    } else {
+      alert('Please sign in to delete a friend.');
+    }
+  };
+
+  handleEditFriend = (id, name) => {
+    // Check if the user is authenticated
+    if (this.state.user) {
+      // Update a friend in the Firebase database
+      firebase
+        .database()
+        .ref(`friends/${id}`)
+        .update({ name });
+    } else {
+      alert('Please sign in to edit a friend.');
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        {/* Render the friends list */}
+      </div>
+    );
+  }
 }
+
+export default class Friends();
+
+
+
+// import React from 'react'
+
+// export default function Friends() {
+//     return(
+//         <div>
+//             <h1>friends page...</h1>
+//         </div>
+//     )
+// }
