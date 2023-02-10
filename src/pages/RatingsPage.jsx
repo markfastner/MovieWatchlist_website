fb0dc318ca8b9aa68866ec5f2938711cb586c084
basicallyphiona <phionanicole.tumbaga@student.csulb.edu>
2023-02-10 11:10:15 -0800

    hi

--- src/pages/RatingsPage.jsx
+++ src/pages/RatingsPage.jsx
@@ -1,8 +1,13 @@
+import "../features/ratingspage/ratings.css";
+
 function RatingsPage() {
   return (
-
+    
   <div class="comments">
-    <h3>Comments</h3>
+    <h1 class="header">
+      Comments and Reactions
+    </h1>
+    
     <form id="add-comment-form">
       <textarea id="new-comment" rows="4" cols="50"></textarea>
       <button type="submit">Add Comment</button>
