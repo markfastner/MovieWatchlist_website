fc49cc125557ee0f274006021169876e93846b29
basicallyphiona <80723239+basicallyphiona@users.noreply.github.com>
2023-02-10 00:47:09 -0800

    Update RatingsPage.jsx

--- src/pages/RatingsPage.jsx
+++ src/pages/RatingsPage.jsx
@@ -1,11 +1,14 @@
 function RatingsPage() {
-    return (
-      <div>
-        <h1>
-        Ratings Page
-        </h1>
-      </div>
-    );
+  return (
+
+  <div class="comments">
+    <h3>Comments</h3>
+    <form id="add-comment-form">
+      <textarea id="new-comment" rows="4" cols="50"></textarea>
+      <button type="submit">Add Comment</button>
+    </form>
+  </div>
+  );
 }
 
 export default RatingsPage;
