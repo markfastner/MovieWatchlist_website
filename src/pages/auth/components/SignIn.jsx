d8c4cfd3c7f6e981c7f3f4801cccc418e1a74bfc
DecimBlade <62186672+DecimBlade@users.noreply.github.com>
2023-02-09 21:47:15 -0800

    Update SignIn.jsx

--- src/pages/auth/components/SignIn.jsx
+++ src/pages/auth/components/SignIn.jsx
@@ -3,28 +3,40 @@ import React from "react";
 
 export default function SignInPage() {
     return(
-        <div>
-            <div class="container">
-        <div class="card">            
-            <div class="card-header" style={{fontWeight:'bold'}}>Sign In</div>    
-            <div class="card-body">
-                <form action="/auth/login" method="GET">    
-                        <label for="email-reg" class="form-label">Email</label>
-                        <br></br>
-                        <input type="text" style={{border: '3px solid rgba(0, 0, 0, 0.7)'}} class="form-control" id="email-reg" name="email"/>                        
-                        <br></br>
-                        <label for="password-reg" class="form-label">Password</label>
-                        <br></br>
-                        <input type="password" style={{border: '3px solid rgba(0, 0, 0, 0.7)'}}  class="form-control" id="password-reg" name="password"/>
-                        <br></br>
-                        <br></br>
-                    <button type="submit" style={{background: 'black', color : 'white'}} class="btn btn-primary">Submit</button>
-                    <br></br>
-                    <a href='/signup'>Don't have an account? Sign Up!</a>
-                </form>
+    <div className="flex justify-end relative min-h-screen bg-no-repeat w-full bg-cover bg-blue-200">
+        <form action="/auth/login" method="GET" className="relative-right-[15%] bg-white shadow-md rounded px-8 pt-6 mb-4">    
+            <div className="mb-4">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="email-reg">
+                Email Address
+            </label>
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email address"
+              type="text"
+              placeholder="Email Address">
+              </input>
             </div>
-        </div>
-    </div>
-</div>
+            <div class="mb-6">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="password">
+              Password
+            </label>
+            <input
+              class="shadow appearance-none border border-black-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Password">
+              </input>
+          </div>
+                
+                
+        <button type="submit" style={{background: 'black', color : 'white'}} class="btn btn-primary">Submit</button>
+        <br></br>
+        <a href='/signup' className="text-blue">Don't have an account? Sign Up!</a>
+        </form>
+    </div>    
     )
 }
