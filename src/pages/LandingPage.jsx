896d25f825c697717236215221d03d5b428304e6
DecimBlade <62186672+DecimBlade@users.noreply.github.com>
2023-02-11 15:10:21 -0800

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -11,72 +11,9 @@ function LandingPage() {
   return (
     <div
       className="flex justify-end items-center relative min-h-screen bg-no-repeat w-full bg-cover 
-        bg-[url('/public/images/HomeLoginImages.jpg')]"
-    >
-        <form action="/auth/login" method="GET" class="relative right-[15%] bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <div class="mb-4">
-            <label
-              class="block text-gray-700 text-sm font-bold mb-2"
-              for="username"
-            >
-              Username
-            </label>
-            <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="username"
-              type="text"
-              placeholder="Username"
-            ></input>
-          </div>
-          <div class="mb-6">
-            <label
-              class="block text-gray-700 text-sm font-bold mb-2"
-              for="password"
-            >
-              Password
-            </label>
-            <input
-              class="shadow appearance-none border border-black-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-              type="password"
-              placeholder="Password"
-            ></input>
-          </div>
-
-          <div class="flex items-center">
-            <input
-              id="link-checkbox"
-              type="checkbox"
-              value=""
-              class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-            ></input>
-            <label
-              for="link-checkbox"
-              class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
-              I agree with the{" "}
-              <a
-                href="/terms-and-conditions"
-                class="text-blue-600 dark:text-blue-500 hover:underline"
-              >
-                terms and conditions
-              </a>
-              .
-            </label>
-            <div class="flex items-center justify-between">
-              
-            </div>
-          </div>
-          <button type="submit"
-                onClick={signIn}
-                class="my-2 w-full duration-200 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
-              >
-                Sign In
-              </button>
-          <p class="text-center text-gray-500 text-xs">
-          &copy;491B Runtime Group
-          </p>
-        </form>
+        bg-[url('/public/images/HomeLoginImages.jpg')]">
+        
+        
       </div>
 
   );
