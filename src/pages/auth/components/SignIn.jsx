4e53c737cb88d396262f1f1f6f97a96274a7670c
DecimBlade <62186672+DecimBlade@users.noreply.github.com>
2023-02-11 11:34:43 -0800

    committing changes pull these one

--- src/pages/auth/components/SignIn.jsx
+++ src/pages/auth/components/SignIn.jsx
@@ -5,7 +5,10 @@ export default function SignInPage() {
     return(
     <div className="flex justify-center items-center relative min-h-screen bg-no-repeat w-full bg-cover bg-blue-200">
         <form action="/auth/login" method="GET" className="relative-right-[15%] bg-white shadow-md rounded px-8 pt-6 mb-4">    
-            <div className="mb-6">
+            <div className="flex justify-center">
+                <img src="[url('/public/images/Logo.png')]" alt="Logo" className="w-14 h-14"></img>
+            </div>
+            <div className="mb-4">
                 <label
                 class="block text-gray-700 text-sm font-bold mb-2"
                 for="email-reg">
@@ -18,24 +21,23 @@ export default function SignInPage() {
                     placeholder="Email Address">
                 </input>
             </div>
-            <div class="mb-6">
+            <div class="mb-4">
                 <label
                 class="block text-gray-700 text-sm font-bold mb-2"
                 for="password">
                 Password
                 </label>
                 <input
-                class="shadow appearance-none border border-black-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                id="password"
-                type="password"
-                placeholder="Password">
+                    class="shadow appearance-none border border-black-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                    id="password"
+                    type="password"
+                    placeholder="Password">
                 </input>
             </div>
 
             <button type="submit" class="btn btn-primary my-2 w-full duration-200 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" >Sign In</button>
             <br></br>
             <a href='/signup' className="text-black">Don't have an account? Sign Up!</a>
-
             <p class="text-center text-gray-500 text-xs">
             &copy;Runtime Group
             </p>
--- src/pages/auth/components/SignUp.jsx
+++ src/pages/auth/components/SignUp.jsx
@@ -2,37 +2,52 @@ import React from 'react'
 
 export default function SignUp(){
     return(
-        <div>
-            <div class="container">
-        <div class="card">            
-            <div class="card-header" style={{fontWeight:'bold'}}>Sign Up</div>    
-            <div class="card-body">
-                <form action="/auth/register" method="POST">
-                    
-                        <label for="name-reg" class="form-label">Name</label>
-                        <br></br>
-                        <input type="text" style={{border: '3px solid rgba(0, 0, 0, 0.7)'}} class="form-control" id="name-reg" name="name"/>                        
-                    
-                        <br></br>
-                        <label for="email-reg" class="form-label">Email</label>
-                        <br></br>
-                        <input type="text" style={{border: '3px solid rgba(0, 0, 0, 0.7)'}} class="form-control" id="email-reg" name="email"/>                        
-                        <br></br>
-                        <label for="password-reg" class="form-label">Password</label>
-                        <br></br>
-                        <input type="password" style={{border: '3px solid rgba(0, 0, 0, 0.7)'}}  class="form-control" id="password-reg" name="password"/>
-                        <br></br>
-                        <label for="password-conf-reg" class="form-label">Confirm Password</label>
-                        <br></br>
-                        <input type="password" style={{border: '3px solid rgba(0, 0, 0, 0.7)'}} class="form-control" id="password-conf-reg" name="password-confirm"/>
-                    <br></br>
-                    <button type="submit" style={{background: 'black', color : 'white'}} class="btn btn-primary">Submit</button>
-                    <br></br>
-                    <a href='/signin'>Already have an account? Sign In.</a>
-                </form>
-            </div>
-        </div>
-    </div>
+        <div className="flex justify-center items-center relative min-h-screen bg-no-repeat w-full bg-cover bg-blue-200">
+            <form action="/auth/signup" method="POST" className="relative-right-[15%] bg-white shadow-md rounded px-8 pt-6 mb-4">
+                <div className="mb-4">
+                    <label
+                    class="block text-gray-700 text-sm font-bold mb-2"
+                    for="email-reg">
+                    Email Address
+                    </label>
+                    <input
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="email address"
+                        type="text"
+                        placeholder="Email Address">
+                    </input>
+                </div>
+                <div class="mb-4">
+                    <label
+                    class="block text-gray-700 text-sm font-bold mb-2"
+                    for="password">
+                    Password
+                    </label>
+                    <input
+                        class="shadow appearance-none border border-black-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        id="password"
+                        type="password"
+                        placeholder="Password">
+                    </input>
+                </div>
+                <div class="mb-4">
+                    <label
+                    class="block text-gray-700 text-sm font-bold mb-2"
+                    for="password">
+                    Confirm Password
+                    </label>
+                    <input
+                        class="shadow appearance-none border border-black-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        id="password"
+                        type="password"
+                        placeholder="Password">
+                    </input>
+                </div>
+            
+            </form>
         </div>
     )
 }
+
+{/* <button type="submit" style={{background: 'black', color : 'white'}} class="btn btn-primary">Submit</button>
+<a href='/signin'>Already have an account? Sign In.</a> */}
