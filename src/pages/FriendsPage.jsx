2dd07d8d71afce4d5f2dc0b1d7b751856b0f93ce
Jake Spievak <jakespvk@gmail.com>
2023-02-11 15:17:44 -0800

    edit friends page, didnt break it

--- src/pages/FriendsPage.jsx
+++ src/pages/FriendsPage.jsx
@@ -1,6 +1,6 @@
-// import firebase from 'firebase/app';
-// import 'firebase/database';
-// import 'firebase/auth';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
 import React from 'react';
 import '../App.css'
 
@@ -16,91 +16,84 @@ function FriendsPage() {
  );
 }
 class Friends extends React.Component {
-    state = {
+  state = {
     friends: [],
     user: null
   };
 
+  componentDidMount() {
+    // Initialize Firebase
+    firebase.initializeApp({
+      apiKey: 'YOUR_API_KEY',
+      authDomain: 'YOUR_AUTH_DOMAIN',
+      databaseURL: 'YOUR_DATABASE_URL',
+    });
 
-//   componentDidMount() {
-//     // Initialize Firebase
-//     firebase.initializeApp({
-//       apiKey: 'YOUR_API_KEY',
-//       authDomain: 'YOUR_AUTH_DOMAIN',
-//       databaseURL: 'YOUR_DATABASE_URL',
-//     });
+    // Listen for changes to the user's authentication state
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user });
+      } else {
+        this.setState({ user: null });
+      }
+    });
 
-//     // Listen for changes to the user's authentication state
-//     firebase.auth().onAuthStateChanged((user) => {
-//       if (user) {
-//         this.setState({ user });
-//       } else {
-//         this.setState({ user: null });
-//       }
-//     });
+    // Listen for changes to the friends list
+    firebase
+      .database()
+      .ref('friends')
+      .on('value', (snapshot) => {
+        this.setState({ friends: snapshot.val() });
+      });
+  }
 
-//     // Listen for changes to the friends list
-//     firebase
-//       .database()
-//       .ref('friends')
-//       .on('value', (snapshot) => {
-//         this.setState({ friends: snapshot.val() });
-//       });
-//   }
+  handleAddFriend = (friend) => {
+    // Check if the user is authenticated
+    if (this.state.user) {
+      // Add a new friend to the Firebase database
+      firebase
+        .database()
+        .ref('friends')
+        .push(friend);
+    } else {
+      alert('Please sign in to add a friend.');
+    }
+  };
 
-//   handleAddFriend = (friend) => {
-//     // Check if the user is authenticated
-//     if (this.state.user) {
-//       // Add a new friend to the Firebase database
-//       firebase
-//         .database()
-//         .ref('friends')
-//         .push(friend);
-//     } else {
-//       alert('Please sign in to add a friend.');
-//     }
-//   };
+  handleDeleteFriend = (id) => {
+    // Check if the user is authenticated
+    if (this.state.user) {
+      // Remove a friend from the Firebase database
+      firebase
+        .database()
+        .ref(`friends/${id}`)
+        .remove();
+    } else {
+      alert('Please sign in to delete a friend.');
+    }
+  };
 
-//   handleDeleteFriend = (id) => {
-//     // Check if the user is authenticated
-//     if (this.state.user) {
-//       // Remove a friend from the Firebase database
-//       firebase
-//         .database()
-//         .ref(`friends/${id}`)
-//         .remove();
-//     } else {
-//       alert('Please sign in to delete a friend.');
-//     }
-//   };
+  handleEditFriend = (id, name) => {
+    // Check if the user is authenticated
+    if (this.state.user) {
+      // Update a friend in the Firebase database
+      firebase
+        .database()
+        .ref(`friends/${id}`)
+        .update({ name });
+    } else {
+      alert('Please sign in to edit a friend.');
+    }
+  };
 
-//   handleEditFriend = (id, name) => {
-//     // Check if the user is authenticated
-//     if (this.state.user) {
-//       // Update a friend in the Firebase database
-//       firebase
-//         .database()
-//         .ref(`friends/${id}`)
-//         .update({ name });
-//     } else {
-//       alert('Please sign in to edit a friend.');
-//     }
-//   };
+  render() {
+    return (
+      <div>
+        {/* Render the friends list */}
+      </div>
+    );
+  }
 }
 
+
 export default FriendsPage;
-  
-
-
-
-
-
-// import React from 'react'
-
-// export default function Friends() {
-//     return(
-//         <div>
-//             <h1>friends page...</h1>
-//         </div>
-//     )
-// }
