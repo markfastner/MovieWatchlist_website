12a6cbdf03bed3e8827c3b95cd8014cd0936e932
DecimBlade <62186672+DecimBlade@users.noreply.github.com>
2023-02-19 11:41:58 -0800

    commits from last night to fix today

--- src/components/navigation/Footer.jsx
+++ src/components/navigation/Footer.jsx
@@ -1,14 +1,39 @@
+import React, {useState, useRef} from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../pages/auth/contexts/AuthContext.js';
+import '../../App.css';
+
+
+
+
 
 
 export const Footer=()=>{
+    const FooterLinks = ["About", "Terms and Conditions", "Support", "Contact Us"]
 
     return(
-        <Footer>
-            <h1>Hello</h1>
-        </Footer>
+        
+        <footer class="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-blue-900">
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="www.runtime.live" class="hover:underline">Runtime™</a>
+            </span>
+            <ul class="flex flex-wrap items-center mt-3 text-sm text-white dark:text-white sm:mt-0">
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+                </li>
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6">Terms and Conditions</a>
+                </li>
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6">Support</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline">Contact Us</a>
+                </li>
+            </ul>
+        </footer>
+
+        
     );
 }
 
