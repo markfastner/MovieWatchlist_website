cdff081beed159ad19c4d80ff8182e6824f2a645
DecimBlade <62186672+DecimBlade@users.noreply.github.com>
2023-02-22 16:19:02 -0800

    font updated plus imported 2 packages

--- src/components/landingpage/Hero.jsx
+++ src/components/landingpage/Hero.jsx
@@ -1,15 +1,18 @@
 // import styles from '../style'
-import React from 'react'
+import React from 'react';
+import WebFont from 'webfontloader';
+// import font from '../components/landingpage/Font.css';
+
 
 const Hero = () => (
-    <section id="home" className = "flex md:flex-row flex-col">
+    <section id="home" className = "flex md:flex-row flex-col font-title">
         <div
             className="flex relative min-h-screen bg-no-repeat w-full bg-cover 
             bg-[url('/public/images/HomeLoginImages.jpg')]">
-            <div className="flex relative mx-auto items-center bg-red-400 flex-col space-y-4 justify-center">
-                <h1 className="text-white text-9xl font-serif font-bold bg-green-500">Entertainment Awaits</h1>
+            <div className="flex relative mx-auto items-center flex-col space-y-4 justify-center">
+                <h1 className="text-blue-400 text-9xl font-serif font-bold">Entertainment Awaits</h1>
                 
-                <button className = "bg-blue-500 border-white text-white text-5xl">
+                <button className = "border-white text-blue-400 text-3xl">
                     Get Started for Free
                 </button>
             </div>
