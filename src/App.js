dc46b2c22a06b59d3d39ba17d4c8c8d5999b055b
Luke Reissmueller <64756162+lreissmueller@users.noreply.github.com>
2023-03-07 18:11:33 -0800

    Inactivity fixed...

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Navbar from './components/navigation/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage'
@@ -9,7 +10,10 @@ import ProfilePage from './pages/ProfilePage'
 import WatchListPage from './pages/WatchListPage'
 import SignIn from './pages/auth/components/SignIn'
 import SignUp from './pages/auth/components/SignUp';
+
 import {AuthProvider, useAuth} from './pages/auth/contexts/AuthContext.js';
+import {auth} from './firebase'
+
 import PrivateRoute from './pages/auth/components/PrivateRoute';
 import SetProfile from './pages/auth/components/SetProfile';
 import ForgotPassword from './pages/auth/components/ForgotPassword';
@@ -18,75 +22,90 @@ import Support from './pages/SupportPage';
 import TermsAndConditions from './pages/TermsAndConditionsPage';
 import About from './pages/AboutMePage';
 import Contact from './pages/ContactPage';
-import { WatchlistProvider } from "./pages/auth/contexts/WatchlistState";
 
 // App component which runs the whole application
 function App() {
   
-  // const {currentUser} = useAuth()
-
-  // // logged in status
-  // const [loggedIn, setLoggedIn] = useState(false)
-  
-  // // check for inactivity and log out
-  // const checkForInactivity = () => {
-
-  //   // Get expiretime from local storage
-  //   const expireTime = localStorage.getItem('expireTime')
-
-  //   // if no user, keep expiretime at 0
-  //   if(currentUser) {
-  //     setLoggedIn(true)
-  //   }
-
-  //   // If expire time is earlier than current time, log out
-  //   if (expireTime < Date.now() && loggedIn) {
-  //     signout()
-  //     setLoggedIn(false)
-  //   }
-  // }
-
-  // // function to update expire time
-  // const updateExpireTime = () => {
-    
-  //   // set expire time to 10 seconds of inactivity, from current time
-  //   const timer = Date.now() + 10000
-
-  //   // set expire time in local storage
-  //   localStorage.setItem('expireTime', timer)
-  // }
-
-  // // use effect to set interval to check for inactivity
-  // useEffect(() => {
+  const [error, setError] = useState("")
 
   
-  //   // check for inactivity every 1 seconds
-  //   const interval = setInterval(() => {
-  //     checkForInactivity()
-  //   }, 1000) 
+  // logs the user out
+  async function handleLogout(){
+    setError('')
+    try {
+      await auth.signOut()
+    } catch {
+      setError = 'Logout not executed.'
+    }
+  }
 
-  //   // clear interval on unmount
-  //   return () => clearInterval(interval)
-  // }, [])
+  // logged in status
+  const [loggedIn, setLoggedIn] = useState(true)
+  
+  // check for inactivity and log out
+  const checkForInactivity = () => {
 
-  // // reset expire time on user activity
-  // useEffect(() => {
+    // Get expiretime from local storage
+    const expireTime = localStorage.getItem('expireTime')
+
+    // if no user, keep expiretime at 0
+    // if(!currentUser) {
+    //   updateExpireTime()
+    //   setLoggedIn(false)
+    // }
+
+    // If expire time is earlier than current time, log out
+    if (expireTime < Date.now()) { //  && loggedIn
+      setLoggedIn(false)
+      handleLogout()
+    }
+  }
+
+  // function to update expire time
+  const updateExpireTime = () => {
     
-  //   // set initial expire time
-  //   updateExpireTime()
+    // set expire time to 10 seconds of inactivity, from current time
+    const expireTime = Date.now() + 1800000
 
-  //   // add event listeners to reset inactivity timer
-  //   window.addEventListener("click", updateExpireTime)
-  //   window.addEventListener("keypress", updateExpireTime)
-  //   window.addEventListener("scroll", updateExpireTime)
-  //   window.addEventListener("mousemove", updateExpireTime)
+    // set expire time in local storage
+    localStorage.setItem('expireTime', expireTime)
+  }
 
-  //   // event listeners must be removed to add new ones
-  //   window.removeEventListener("click", updateExpireTime)
-  //   window.removeEventListener("keypress", updateExpireTime)
-  //   window.removeEventListener("scroll", updateExpireTime)
-  //   window.removeEventListener("mousemove", updateExpireTime)
-  // }, [])
+  // use effect to set interval to check for inactivity
+  useEffect(() => {
+
+  
+    // check for inactivity every 1 seconds
+    const interval = setInterval(() => {
+      checkForInactivity()
+    }, 1000) 
+
+    // clear interval on unmount
+    return () => clearInterval(interval)
+  }, [])
+
+  // reset expire time on user activity
+  useEffect(() => {
+    
+    // set initial expire time
+    updateExpireTime()
+
+    // add event listeners to reset inactivity timer
+    window.addEventListener("click", updateExpireTime)
+    window.addEventListener("keypress", updateExpireTime)
+    window.addEventListener("scroll", updateExpireTime)
+    window.addEventListener("mousemove", updateExpireTime)
+
+    // clean up
+    return() => {
+      
+      // event listeners must be removed to add new ones
+      window.removeEventListener("click", updateExpireTime)
+      window.removeEventListener("keypress", updateExpireTime)
+      window.removeEventListener("scroll", updateExpireTime)
+      window.removeEventListener("mousemove", updateExpireTime)
+    }
+  },  [])
   
 
   // Returns the app component which handles the routing of the application
@@ -94,7 +113,6 @@ function App() {
     <>
         <Router>
           <AuthProvider> 
-          <WatchlistProvider>
           <Navbar/>
           <Routes>
             <Route exact path='/'  element={<LandingPage/>} />
@@ -117,7 +135,6 @@ function App() {
             <Route path='/set-profile' element={<PrivateRoute><SetProfile/></PrivateRoute>}/>
             <Route path='/forgot-password' element={<ForgotPassword/>}/>
           </Routes>
-          </WatchlistProvider>
           </AuthProvider>
           <Footer/>
         </Router>
