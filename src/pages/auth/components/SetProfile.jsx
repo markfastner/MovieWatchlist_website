021558f120f54e5a6e8dd3eb111a5c75c695f4d7
DecimBlade <62186672+DecimBlade@users.noreply.github.com>
2023-04-24 09:15:55 -0700

    testing

--- src/pages/auth/components/SetProfile.jsx
+++ src/pages/auth/components/SetProfile.jsx
@@ -328,14 +328,23 @@ export default function SetProfile() {
                 <option selected>None</option>}
                 <option>Action</option>
                 <option>Adventure</option>
+                <option>Animation</option>
                 <option>Comedy</option>
+                <option>Crime</option>
                 <option>Documentary</option>
                 <option>Drama</option>
+                <option>Family</option>
+                <option>Fantasy</option>
+                <option>History</option>
                 <option>Horror</option>
+                <option>Music</option>
                 <option>Mystery</option>
                 <option>Romance</option>
                 <option>Sci-Fi</option>
+                <option>TV Movie</option>
                 <option>Thriller</option>
+                <option>War</option>
+                <option>Western</option>
             </select>
         </div>
             <button type="submit" class="btn btn-primary my-6 w-full duration-200 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline" >Set Profile</button>
