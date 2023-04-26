3ceb7d80e0dcdaa816e0a032be8eb1b2f9a02121
markfastner <markfastner@hotmail.com>
2023-04-25 23:04:01 -0700

    temp friends fix

--- src/components/chat/Chats.jsx
+++ src/components/chat/Chats.jsx
@@ -30,7 +30,7 @@ const Chats = () => {
 
     return (
     <div className="chats">
-        {Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat) => (
+        {/* {Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat) => (
         <div className='userChat' key={chat[0]} onClick={handleSelect(chat[1].userInfo)}>
             <img src='' alt=''/>
             <div className='userChatInfo'>
@@ -38,7 +38,7 @@ const Chats = () => {
                 <p>{chat[1].lastMessage?.text}</p>
       </div>
         </div>
-        ))}
+        ))} */}
       </div>
   )
 }
