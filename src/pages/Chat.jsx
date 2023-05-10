037c58abe0c9fe216b735f03002b154456195335
Jake Spievak <jakespvk@gmail.com>
2023-05-09 23:44:33 -0700

    fix: orderBy timestamp now works properly

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { collection, query, orderBy, onSnapshot, getDocs } from "firebase/firestore";
 import SendMessage from "./SendMessage";
-import { db, database } from "../firebase.js";
+import { database } from "../firebase.js";
 
 const Chat = () => {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    const messagesRef = db.messages;
-    const q = query(messagesRef, ("timestamp"));
+    const messagesRef = collection(database, "messages");
+    const q = query(messagesRef, orderBy("timestamp"));
     const unsubscribe = onSnapshot(q, (snapshot) => {
       const fetchedMessages = snapshot.docs.map((doc) => ({
         id: doc.id,
